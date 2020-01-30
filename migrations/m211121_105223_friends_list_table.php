8d91154155258300b329e5bf75a769f2b54dc305
Vikas Pandey <svikas.pandey@gmail.com>
2020-01-30 17:49:53 +0530

    Update m211121_105223_friends_list_table.php

--- migrations/m211121_105223_friends_list_table.php
+++ migrations/m211121_105223_friends_list_table.php
@@ -1,15 +1,10 @@
 <?php
-/**
- * @link https://github.com/bubasuma/yii2-simplechat
- * @copyright Copyright (c) 2015 bubasuma
- * @license http://opensource.org/licenses/BSD-3-Clause
- */
 use nextvikas\nextchat\migrations\Migration;
 
 /**
  * Class m151121_105223_user_table
  *
- * @author Buba Suma <bubasuma@gmail.com>
+ * @author Vikas Pandey
  * @since 1.0
  */
 class m211121_105223_friends_list_table extends Migration
